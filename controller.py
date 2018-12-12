f1d9b75d7f25fc3d045ea8ff1ded0c84d9e8687a
Ulysses Yu <ulysses_yu@yahoo.com>
2018-12-11 23:34:49 -0600

    working controller

--- controller.py
+++ controller.py
@@ -97,23 +97,7 @@ class Controller:
 
         return new_x, new_y
 
-    @staticmethod
-    def range_detector(self, other_obj, sqr_range):
-        """
-        Inputs: the current controller, another controller object, and the sqr_range
-        Outputs: a boolean indicating if the two controllers are in communication range
-        The two controllers are in range if they are within sqr_range units of each other both x and y wise
-        Thus, the boundary for each controller looks like a square with length sqr_range * 2
-        Choose sqr_range = 2 for communication, sqr_range = 1 for collision
-        """
-        x_dist = abs(self.x - other_obj.x)
-        y_dist = abs(self.y - other_obj.y)
-
-        if x_dist <= sqr_range and y_dist <= sqr_range:
-            return True
-        else:
-            return False
-
+    
     def collision_free_destination(self):
         """
         This sets the path for the controller if the controller is not in range of another aircraft
@@ -130,60 +114,152 @@ class Controller:
 
             # want to head either north or east
             if self.direction == "S":
-                self.set_dir("E")
+                return "E"
             elif self.direction == "W":
-                self.set_dir("N")
+                return "N"
             elif x_difference > y_difference:
-                self.set_dir("E")
+                return "E"
             else:
-                self.set_dir("N")
+                return "N"
 
         elif x_difference >= 0 and y_difference < 0:
 
             # want to head either south or east
             if self.direction == "W":
-                self.set_dir("S")
+                return "S"
             elif self.direction == "N":
-                self.set_dir("E")
+                return "E"
             elif x_difference > abs(y_difference):
-                self.set_dir("E")
+                return "E"
             else:
-                self.set_dir("S")
+                return "S"
                 
 
         elif x_difference < 0 and y_difference >= 0:
 
             # want to head either north or west
             if self.direction == "E":
-                self.set_dir("N")
+                return "N"
             elif self.direction == "S":
-                self.set_dir("W")
+                return "W"
             elif abs(x_difference) > y_difference:
-                self.set_dir("W")
+                return "W"
             else:
-                self.set_dir("N")
+                return "N"
 
         else:
 
             # want to head either south or west
             if self.direction == "N":
-                self.set_dir("W")
+                return "W"
             elif self.direction == "E":
-                self.set_dir("S")
+                return "S"
             elif abs(x_difference) > abs(y_difference):
-                self.set_dir("W")
+                return "W"
             else:
-                self.set_dir("S")
+                return "S"
 
-## this is the test script
-new_controller = Controller(0, 0, 10, 20)
+    def with_collision_destination(self, other_obj):
+        """
+        This function accounts for potential collisions between the aircrafts in the x-y plane
+        It sets a path by iterating through the list of possible paths for both planes and evaluating if they create
+        unsafe paths.
+        It takes as input the two planes and then returns the direction for both planes
+        This function does not take into account route planning
+        """
+        possible_paths = [("L", "F"), ("L", "R"),
+                            ("F", "L"), ("F", "F"), ("F", "R"),
+                            ("R", "L"), ("R", "F")]
+        
+        cont1_curdir = self.direction
+        cont2_curdir = other_obj.get_dir()
+        i = 0
+        colliding = True
+
+        while i < len(possible_paths) and colliding:
+
+            if possible_paths[i][0] == "L":
+                cont1_curdir = self.calc_left_turn(cont1_curdir)
+            elif possible_paths[i][0] == "R":
+                cont1_curdir = self.calc_right_turn(cont1_curdir)
+            
+            if possible_paths[i][1] == "L":
+                cont2_curdir = other_obj.calc_left_turn(cont2_curdir)
+            elif possible_paths[i][1] == "R":
+                cont2_curdir = other_obj.calc_right_turn(cont2_curdir)
+
+            new1_x, new1_y = self.next_location(self.get_x(), self.get_y(), cont1_curdir)
+            new2_x, new2_y = self.next_location(other_obj.get_x(), other_obj.get_y(), cont2_curdir)
+
+            colliding = range_detector(new1_x, new1_y, new2_x, new2_y, 1)
+            i = i + 1
+
+        return cont1_curdir, cont2_curdir    
+            
+#############################################
+# END CLASS
+#############################################
+
+def range_detector(first_x, first_y, second_x, second_y, sqr_range):
+    """
+    Inputs: the current controller, another controller object, and the sqr_range
+    Outputs: a boolean indicating if the two controllers are in communication range
+    The two controllers are in range if they are within sqr_range units of each other both x and y wise
+    Thus, the boundary for each controller looks like a square with length sqr_range * 2
+    Choose sqr_range = 2 for communication, sqr_range = 1 for collision
+    """
+    x_dist = abs(first_x - second_x)
+    y_dist = abs(first_y - second_y)
+
+    if x_dist <= sqr_range and y_dist <= sqr_range:
+        return True
+    else:
+        return False
+
+## this is the executions script
+new_controller = Controller(0, 10, 0, -1)
 new_controller.set_dir("S")
+cont_2 = Controller(0, -10, 0, 1)
+cont_2.set_dir("W")
 
-while not(new_controller.found_destination()):
+while not(new_controller.found_destination()) or not(cont_2.found_destination()):
 
-    new_controller.collision_free_destination()
-    new_x, new_y = new_controller.next_location(new_controller.get_x(), new_controller.get_y(), new_controller.get_dir())
-    print("(" + str(new_x) + ", " + str(new_y) + ")")
-    # print("This is the new x: " + str(new_x) + " This is the new y: " + str(new_y))
-    new_controller.set_location(new_x, new_y)
+    comm_range = range_detector(new_controller.get_x(), new_controller.get_y(), cont_2.get_x(), cont_2.get_y(), 2)
+    cont1_dest = new_controller.found_destination()
+    cont2_dest = cont_2.found_destination()
 
+    # checks if the controllers are in communication range
+    if comm_range and not(cont1_dest) and not(cont2_dest):
+        # print("Communication Range")
+        cont1_path, cont2_path = new_controller.with_collision_destination(cont_2)
+
+        # sets path to avoid collisions
+        new_controller.set_dir(cont1_path)
+        cont_2.set_dir(cont2_path)
+
+        # calculates new locations and sets them
+        new_x, new_y = new_controller.next_location(new_controller.get_x(), new_controller.get_y(), new_controller.get_dir())
+        cont2_x, cont2_y = cont_2.next_location(cont_2.get_x(), cont_2.get_y(), cont_2.get_dir())
+        new_controller.set_location(new_x, new_y)
+        cont_2.set_location(cont2_x, cont2_y)
+
+    else:
+        # print("Not Communicating")
+
+        # sets the path of controller one
+        if not(cont1_dest):
+            new_controller.set_dir(new_controller.collision_free_destination())
+            new_x, new_y = new_controller.next_location(new_controller.get_x(), new_controller.get_y(), new_controller.get_dir())
+            new_controller.set_location(new_x, new_y)
+
+        # sets the path of controller two
+        if not(cont2_dest):
+            cont_2.set_dir(cont_2.collision_free_destination())
+            cont2_x, cont2_y = cont_2.next_location(cont_2.get_x(), cont_2.get_y(), cont_2.get_dir())
+            cont_2.set_location(cont2_x, cont2_y)
+
+    if range_detector(new_controller.get_x(), new_controller.get_y(), cont_2.get_x(), cont_2.get_y(), 1) and not(cont1_dest) and not(cont2_dest):
+        print("Planes collided!")
+
+    print("Plane 1: (" + str(new_controller.get_x()) + ", " + str(new_controller.get_y()) + 
+        ") Plane 2: (" + str(cont_2.get_x()) + ", " + str(cont_2.get_y()) + ")")
